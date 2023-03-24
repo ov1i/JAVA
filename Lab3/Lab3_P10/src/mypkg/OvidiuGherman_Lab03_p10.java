fb0d9be987d07042823a65e7607122f7adb6bd3d
Ovidiu Gherman <govidiu15@gmail.com>
2023-03-24 14:06:42 +0200

    Revert "Lab3-P10 Completed&Comment"
    This reverts commit 6311019c34cd6f706aa2b15f8eb279c21e9d4d86.

--- Lab3/Lab3_P10/src/mypkg/OvidiuGherman_Lab03_p10.java
+++ Lab3/Lab3_P10/src/mypkg/OvidiuGherman_Lab03_p10.java
@@ -3,19 +3,6 @@ package mypkg;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * Assume that there is a cryptographic algorithm which takes an input text ‘A’ composed of lower and upper case
- * characters. Separately a character string ‘B’ is defined. Each character from B has an associated random integer
- * value between 1 and 100. The algorithm checks if the letters from B are found in A and adds the associated
- * numerical values. To the final sum value, the algorithm also adds the positions from string A where characters
- * from string B were found. If the final sum is larger than 100, the encryption was valid. Display a message with the result.
- * 			Example:
- * 			 - String A = ”aTmPpDsst”
- * 			 - String B =”ams”
- * 			 - Associated numerical values for string B: 11 33 7
- * 			 - Sum: (11+33+7+7)+(1+3+7+8)=77 -> INVALID ENCRYPTION
- */
-
 public class OvidiuGherman_Lab03_p10 {
 	static boolean encrypt(String A, String B, int[] val) {
 		int sum = 0;
