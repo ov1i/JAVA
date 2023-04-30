8f9544d8697a4f750ebfe7d1c24b972d0880c058
Ovi <govidiu15@gmail.com>
2023-04-30 04:40:36 +0300

    Lab7 -ONGOING

--- Lab7/Lab7_P5/src/main/OvidiuGherman_Lab07_P05.java
+++ Lab7/Lab7_P5/src/main/OvidiuGherman_Lab07_P05.java
@@ -1,24 +1,118 @@
 package main;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-/* 2.
- * Write a class called Calculator which has the methods to do addition, subtraction, multiplication and division. The
- * methods will take generic input variables and will return the corresponding type. For example, the sum of two integers
- * should return an integer, and for floats it should return a float. Same for division. Adding and subtracting is allowed for
- * String variables as well, but the multiplication and division will print an error message.
- * Write the same class, but use method overloading.
+/* 5. 
+ * Write a class named Student that has the private fields name, group, average_mark, and getter / setter methods for
+ * all the attributes. In the main() method, placed in another class, create a sortedSet collection, with Student type objects,
+ * initialized with values read from the keyboard, that will keep the elements in descending order by average_mark and in
+ * ascending order by name (the students that have the same average will appear in alphabetic order). Browse the
+ * collection using for-loop and display all the items. Then browse the collection with an iterator and display all students
+ * with average_mark> = 8. Browse the collection with forEach () and display all student data in a particular group.
  */
 
 
-class Calculator {
+class Student {
+	private String name;
+	private int group;
+	private float average_mark;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGroup() {
+		return group;
+	}
+	public void setGroup(int group) {
+		this.group = group;
+	}
+	public float getAverage_mark() {
+		return average_mark;
+	}
+	public void setAverage_mark(float average_mark) {
+		this.average_mark = average_mark;
+	}
 	
 }
 
+
 public class OvidiuGherman_Lab07_P05 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Please enter the number of students: ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		
+		Comparator<Student> comp = new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				if(o1.getAverage_mark() > o2.getAverage_mark()){				
+					return -1;
+				}
+				else if(o1.getAverage_mark() < o2.getAverage_mark()) {				
+					return 1;
+				}			
+				return o1.getName().compareToIgnoreCase(o2.getName());
+			}
+		};
+						
+		SortedSet<Student> set = new TreeSet<>(comp);
+		
+		for(int i = 0; i < n;i ++) {
+			Student tempStudent = new Student();
+			System.out.println("Student " + (i+1) + ": ");
+			System.out.print("Please enter the name of the student: "); 
+			tempStudent.setName(scanner.nextLine());
+			do {
+				System.out.print("Please enter the average mark of the student: ");
+				tempStudent.setAverage_mark(scanner.nextFloat());
+			} while(tempStudent.getAverage_mark() > 10.0f || tempStudent.getAverage_mark() <= 0.0f);
+			System.out.print("Please enter the group of the student: ");
+			tempStudent.setGroup(scanner.nextInt());
+			scanner.nextLine();
+			set.add(tempStudent);
+		}
+		
+		System.out.println("\nOutput(#1): ");
+		for(Student obj : set) {
+			System.out.println("Name: " + obj.getName());
+			System.out.println("Average mark: " + obj.getAverage_mark());
+			System.out.println("Group: " + obj.getGroup());
+		}
+		
+		Iterator<Student> iterator = set.iterator();
+		System.out.println("\nOutput(#2): ");
+		System.out.println("->The students with average mark >= 8.0 are: ");
+		while(iterator.hasNext()) {
+			Student tempObj = iterator.next();
+			if(tempObj.getAverage_mark() >= 8.0f ) {
+				System.out.println("Name: " + tempObj.getName());
+				System.out.println("Average mark: " + tempObj.getAverage_mark());
+				System.out.println("Group: " + tempObj.getGroup());
+			}
+		}
+		System.out.print("Please enter a desired group: ");
+		int group = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("\nOutput(#2): ");
+		System.out.println("->The students within group " + group + " are: ");
+		set.forEach(o -> {
+			if(o.getGroup() == group) {
+				System.out.println("Name: " + o.getName());
+				System.out.println("Average mark: " + o.getAverage_mark());
+				System.out.println("Group: " + o.getGroup());
+			}
+		});
 	}
 
 }
--- Lab7/Lab7_P7/src/main/OvidiuGherman_Lab07_P07.java
+++ Lab7/Lab7_P7/src/main/OvidiuGherman_Lab07_P07.java
@@ -1,10 +1,233 @@
 package main;
 
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/* 7. 
+ * Create a generic class called VirtualLibrary that manages a SortedSet of entries. The types of entries are Book, Article,
+ * MediaResource, Magazine and Manual. Implement the specific classes for each type of entry, each of them being a subclass of Publication.
+ * The VirtualLibrary has methods to add, add multiple, return a specific entry and check if an entry exists in the library.
+ * In the main() method, create an instance of VirtualLibrary and use the offered facilities.
+ */
+
+class VirtualLibrary <T extends Publication> {
+	private SortedSet<T> entries;
+
+	public VirtualLibrary() {
+		entries = new TreeSet<>();
+	}
+	public void add(T entry) {
+		entries.add(entry);
+	}
+	public void add_multiple(TreeSet<T> entries) {
+		this.entries.addAll(entries);
+	}
+	@SuppressWarnings("unchecked")
+	public T getEntry(int i) throws ArrayIndexOutOfBoundsException{
+		return (T) entries.toArray()[i];
+	}
+	public boolean checkEntry(T entry) {
+		return entries.contains(entry);
+	}
+}
+
+class Publication implements Comparable<Publication>{
+	private String name;
+	@Override
+	public int compareTo(Publication o) {		
+		return this.getName().compareTo(o.getName());
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+class Book extends Publication{}
+class Article extends Publication{}
+class MediaResource extends Publication{}
+class Magazine extends Publication{}
+class Manual extends Publication{}
+
 public class OvidiuGherman_Lab07_P07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		VirtualLibrary<Publication> e_Library = new VirtualLibrary<>();
+		Publication obj;
+		String input;
+		boolean running = true;
+		System.out.println("Type \"help\" to see the avialable commands");
+		while(running) {
+			System.out.print("Please enter a command: ");
+			input = scanner.nextLine();
+			switch(input.toLowerCase()) {
+			case "bye":
+				System.out.println("Goodbye!");
+				running = false;
+				break;
+			case "book":
+				obj = new Book();
+				System.out.print("Please enter the name of the book: ");
+				obj.setName(scanner.nextLine());
+				e_Library.add(obj);
+				break;
+			case "article": 
+				obj = new Article();
+				System.out.print("Please enter the name of the article: ");
+				obj.setName(scanner.nextLine());
+				e_Library.add(obj);
+				break;
+			case "mediaresource": 
+				obj = new MediaResource();
+				System.out.print("Please enter the name of the media resource: ");
+				obj.setName(scanner.nextLine());
+				e_Library.add(obj);
+				break;
+			case "magazine": 
+				obj = new Magazine();
+				System.out.print("Please enter the name of the magazine: ");
+				obj.setName(scanner.nextLine());
+				e_Library.add(obj);
+				break;
+			case "manual": 
+				obj = new Manual();
+				System.out.print("Please enter the name of the manual: ");
+				obj.setName(scanner.nextLine());
+				e_Library.add(obj);
+				break;	
+			case "check": 
+				System.out.print("Please enter what product you are looking for: ");
+				String product = scanner.nextLine();
+				switch(product.toLowerCase()) {
+				case "book":
+					obj = new Book();
+					System.out.print("Please enter the name of the book you are searching for: ");
+					obj.setName(scanner.nextLine());			
+					if(e_Library.checkEntry(obj)) {
+						System.out.println("We have the book in stock!");
+					}
+					else {
+						System.out.println("I'm sorry we don't have the book in stock!");
+					}
+					break;
+				case "article":
+					obj = new Article();
+					System.out.print("Please enter the name of the article you are searching for: ");
+					obj.setName(scanner.nextLine());			
+					if(e_Library.checkEntry(obj)) {
+						System.out.println("We have the article in stock!");
+					}
+					else {
+						System.out.println("I'm sorry we don't have the article in stock!");
+					}
+					break;
+				case "mediaresource":
+					obj = new MediaResource();
+					System.out.print("Please enter the name of the media resource you are searching for: ");
+					obj.setName(scanner.nextLine());			
+					if(e_Library.checkEntry(obj)) {
+						System.out.println("We have the media resource in stock!");
+					}
+					else {
+						System.out.println("I'm sorry we don't have the media resource in stock!");
+					}
+					break;
+				case "magazine":
+					obj = new Magazine();
+					System.out.print("Please enter the name of the magazine you are searching for: ");
+					obj.setName(scanner.nextLine());			
+					if(e_Library.checkEntry(obj)) {
+						System.out.println("We have the magazine in stock!");
+					}
+					else {
+						System.out.println("I'm sorry we don't have the magazine in stock!");
+					}
+					break;
+				case "manual":
+					obj = new Manual();
+					System.out.print("Please enter the name of the manual you are searching for: ");
+					obj.setName(scanner.nextLine());			
+					if(e_Library.checkEntry(obj)) {
+						System.out.println("We have the manual in stock!");
+					}
+					else {
+						System.out.println("I'm sorry we don't have the manual in stock!");
+					}
+					break;		
+				}
+				break;
+			case "get" :
+				System.out.print("Please tell me the the location of the desired product(int): ");
+				int index = scanner.nextInt();
+				scanner.nextLine();
+				try {
+					System.out.println("The title of the product is: " + e_Library.getEntry(index).getName());
+				} catch(ArrayIndexOutOfBoundsException e) {
+					System.out.println("I'm sorry we don't have such a location in our library!");
+				}
+				break;
+			case "addmultiple":
+				System.out.print("Please enter how many products you want to add in the library: ");
+				int count = scanner.nextInt();
+				scanner.nextLine();
+				System.out.println("Please enter what type of products we'll add: ");
+				String prod = scanner.nextLine();				
+				TreeSet<Publication> list = new TreeSet<>();
+				switch(prod.toLowerCase()) {
+				case "book":
+					for(int i = 0; i < count;i++) {
+						obj = new Book();
+						System.out.print("Please enter the name of the book " + (i+1) + " : ");
+						obj.setName(scanner.nextLine());	
+						list.add(obj);
+					}
+					break;
+				case "article":
+					for(int i = 0; i < count;i++) {
+						obj = new Article();
+						System.out.print("Please enter the name of the article " + (i+1) + " : ");
+						obj.setName(scanner.nextLine());	
+						list.add(obj);
+					}
+					break;
+				case "mediaresource":
+					for(int i = 0; i < count;i++) {
+						obj = new MediaResource();
+						System.out.print("Please enter the name of the media resource " + (i+1) + " : ");
+						obj.setName(scanner.nextLine());	
+						list.add(obj);
+					}
+					break;
+				case "magazine":
+					for(int i = 0; i < count;i++) {
+						obj = new Magazine();
+						System.out.print("Please enter the name of the magazine " + (i+1) + " : ");
+						obj.setName(scanner.nextLine());	
+						list.add(obj);
+					}
+					break;
+				case "manual":
+					for(int i = 0; i < count;i++) {
+						obj = new Manual();
+						System.out.print("Please enter the name of the manual " + (i+1) + " : ");
+						obj.setName(scanner.nextLine());	
+						list.add(obj);
+					}
+					break;
+			}
+			e_Library.add_multiple(list);
+			break;
+		case "help":
+			System.out.println("Commands: Book, Article, MediaResource, Magazine, Manual, Get, Check, AddMultiple, Bye");
+			break;
+		}
 	}
+
+	scanner.close();
+}
 
 }
