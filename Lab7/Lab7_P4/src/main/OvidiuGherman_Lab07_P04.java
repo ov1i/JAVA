937bbb822a954d5c6cd2650fb520c0e5cb531997
Ovidiu Gherman <govidiu15@gmail.com>
2023-05-01 00:05:14 +0300

    Lab Done

--- Lab7/Lab7_P4/src/main/OvidiuGherman_Lab07_P04.java
+++ Lab7/Lab7_P4/src/main/OvidiuGherman_Lab07_P04.java
@@ -1,4 +1,9 @@
-package main;
+package mypkg;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Hashtable;
+import java.util.Scanner;
 
 /* 4. 
  * Implement a class called UserFile (name, extension, type, size in bytes, constructors, mutators, accesors). The types of
@@ -8,96 +13,106 @@ package main;
  */
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+class UserFile {
+	private String name;
+	private String extension;
+	private String type;
+	private Hashtable<String, Integer> types;
+	private int sizeInBytes;
+
+	public UserFile() {
+		this.name = null;
+		this.type = null;
+		this.extension = null;
+		this.types = new Hashtable<String, Integer>() {{ 
+			put("imgae", 0);
+			put("executable", 1);
+			put("binary", 2);
+			put("text", 3);
+		}};
+		this.sizeInBytes = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getExtension() {
+		return extension;
+	}
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type.toLowerCase();
+	}
+	public int getSizeInBytes() {
+		return sizeInBytes;
+	}
+	public void setSizeInBytes(int sizeInBytes) {
+		this.sizeInBytes = sizeInBytes;
+	}
+	public boolean checkType() {
+		return this.types.containsKey((String)this.type);
+	}
+}
 
 public class OvidiuGherman_Lab07_P04 {
-    public static void main(String[] args) {
-        ArrayList<UserFile> files = addFiles();
-        orderFilesAndShow(files);
-    }
-
-    private static ArrayList<UserFile> addFiles() {
-        ArrayList<UserFile> files = new ArrayList<>();
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number of files: ");
-            int noFiles = scanner.nextInt();
-            for (int i = 0; i < noFiles; i++) {
-                System.out.printf("\nFile %d\n", i + 1);
-                System.out.print("Name: ");
-                String name = scanner.next();
-                System.out.print("Extension: ");
-                String extension = scanner.next();
-                System.out.print("Type (image, text or application): ");
-                String type = scanner.next();
-                while (!isValidType(type)) {
-                    System.out.println("\nNot a valid input!\nTry again!");
-                    System.out.print("Type (image, text or application): ");
-                    type = scanner.next();
-                }
-                System.out.print("Enter file size in bytes: ");
-                int sizeInBytes = scanner.nextInt();
-                UserFile file = new UserFile(name, extension, type, sizeInBytes);
-                files.add(file);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return files;
-    }
-
-    private static boolean isValidType(String type) {
-        return type.equalsIgnoreCase("image")
-                || type.equalsIgnoreCase("text")
-                || type.equalsIgnoreCase("application");
-    }
-
-    private static void orderFilesAndShow(ArrayList<UserFile> files) {
-        System.out.println("\nYour ordered list: ");
-        Collections.sort(files);
-        for (UserFile file : files) {
-            System.out.printf("\nFile name: %s.%s\n", file.getName(), file.getExtension());
-            System.out.printf("File size: %d bytes\n", file.getSizeInBytes());
-        }
-    }
+	public static void main(String[] args) {
+		
+		Comparator<UserFile> comparator = new Comparator<UserFile>() {
+			@Override
+			public int compare(UserFile f1, UserFile f2) {
+				if(f1.getSizeInBytes() > f2.getSizeInBytes())
+					return 1;
+				else if(f1.getSizeInBytes() < f2.getSizeInBytes())			
+					return -1;		
+				else
+					return 0;
+			}
+		};
+		
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<UserFile> files = new ArrayList<>();
+		System.out.print("Please enter the number of files: ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		for (int i = 0; i < n; i++) {
+			UserFile tempObj = new UserFile();
+			System.out.print("File " + (i + 1) + "\n");
+			System.out.print("Please enter the name of the file: ");
+			tempObj.setName(scanner.nextLine());
+			System.out.print("Please enter the extension of the file: ");
+			tempObj.setExtension(scanner.nextLine());
+			System.out.print("Please enter the type of the file(Predefined values: Image, Binary, Executable, Text): ");
+			tempObj.setType(scanner.nextLine());
+			System.out.print("Enter file size in bytes: ");
+			tempObj.setSizeInBytes(scanner.nextInt());
+			scanner.nextLine();
+			
+			files.add(tempObj);
+		}
+		scanner.close();
+		
+		System.out.println("Predefined data extension: ");
+		for (UserFile o : files) {
+			if(o.checkType()) {
+				 System.out.print("File: "+o.getName() + " -> Type: " + o.getType() + " -> Extension: " + o.getExtension() + "\n");
+			}
+		}
+		
+		
+		System.out.println("Sorted list: ");
+		files.sort(comparator);
+		for (UserFile o : files)
+	         System.out.print("File: "+o.getName() + " -> Size: " + o.getSizeInBytes() + " bytes\n");
+	}
 }
 
-class UserFile implements Comparable<UserFile> {
-    private String name;
-    private String extension;
-    private String type;
-    private int sizeInBytes;
-
-    public UserFile(String name, String extension, String type, int sizeInBytes) {
-        this.name = name;
-        this.extension = extension;
-        this.type = type;
-        this.sizeInBytes = sizeInBytes;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getExtension() {
-        return extension;
-    }
-
-    public int getSizeInBytes() {
-        return sizeInBytes;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public int compareTo(UserFile file) {
-        if (this.getSizeInBytes() != file.getSizeInBytes()) {
-            return this.getSizeInBytes() - file.getSizeInBytes();
-        } else {
-            return this.getName().compareTo(file.getName());
-        }
-    }
-}
