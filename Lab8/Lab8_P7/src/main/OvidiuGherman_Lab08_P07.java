9239a513acc9a3dc7c1c111ae036297e08496e76
Ovi <govidiu15@gmail.com>
2023-05-04 23:01:23 +0300

    Lab -ONGOING

--- Lab8/Lab8_P7/src/main/OvidiuGherman_Lab08_P07.java
+++ Lab8/Lab8_P7/src/main/OvidiuGherman_Lab08_P07.java
@@ -1,5 +1,13 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
 /* 7. 
  * Write a Java application which enables the serialization and deserialization of objects that represent arrays of int
  * values. Populate an object with keyboard entered data, order the values and store the object in a file. Read the file and
@@ -9,7 +17,69 @@ package main;
 public class OvidiuGherman_Lab08_P07 {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		while(true) {
+			System.out.print("Please enter a command(serialize,deserialize,exit): ");
+			String input = scanner.next();
+
+			if(input.equalsIgnoreCase("serialize")) {
+				System.out.print("Please enter the size of the integer array :");
+				int size = scanner.nextInt();
+				Integer array[] = new Integer[size];
+
+				System.out.print("Please enter the elements of the array: ");
+
+				for(int i = 0; i < array.length;i++) {
+					array[i] = scanner.nextInt();
+				}
+
+				System.out.println("Serialization started!");
+
+				FileOutputStream outFile;
+				try {
+					outFile = new FileOutputStream("output.bin");
+					ObjectOutputStream outputSerializedStreamObj = new ObjectOutputStream (outFile);
+					outputSerializedStreamObj.writeObject(array);
+					outputSerializedStreamObj.close();
+					outFile.close();
+				} catch (IOException e1) {
+					System.out.println(e1.getMessage());
+					continue;
+				}
+
+				System.out.println("Serialization finished!");
+
+			} else if(input.equalsIgnoreCase("deserialize")) {
+				try {
+					FileInputStream inputFile =new FileInputStream ("output.bin");
+					ObjectInputStream inputSerializedStreamObj = new ObjectInputStream (inputFile);
+					Integer deserializedIntData[] = (Integer[]) inputSerializedStreamObj.readObject();
+					System.out.print("Deserialized data: ");
+					for(Integer obj : deserializedIntData) {
+						System.out.print(obj + " ");
+					}
+					inputSerializedStreamObj.close();
+					inputFile.close();
+				} catch (FileNotFoundException e) {
+					System.out.println(e.getMessage() + "\nPlease create a serialized file first!");
+					continue;
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+					continue;
+				} catch (ClassNotFoundException e) {					
+					System.out.println(e.getMessage());
+					continue;
+				}
+				System.out.println("\nDeserialization finished!");
+			} else if(input.equalsIgnoreCase("exit")) {
+				System.out.println("Goodbye!");
+				break;
+			} else { 
+				System.out.println("UNKOWN COMMAND!");
+			}		
+		}
+		scanner.close();
 	}
 
 }
--- Lab8/Lab8_P8/src/main/OvidiuGherman_Lab08_P08.java
+++ Lab8/Lab8_P8/src/main/OvidiuGherman_Lab08_P08.java
@@ -1,5 +1,13 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 //8. 
 //	Write a Java application which reads a file with the following format:
 //	*/rnd1_001.lab
@@ -19,10 +27,46 @@ package main;
 //	Separate the information from the file into distinct files which are named according to the line which starts with */ .
 
 public class OvidiuGherman_Lab08_P08 {
-
+	public static boolean checkNewFile(String line) {
+		if(line.contains("*/")) {
+			return true;
+		}
+		return false;
+	}
 	public static void main(String[] args) {
-	
-
+		BufferedReader buffer = null;				
+		BufferedWriter outputFile = null;
+		try {
+			buffer = new BufferedReader(new FileReader("input.txt"));	
+		} catch (FileNotFoundException e) {
+			System.out.println("Failed to read the file!");
+		} finally {
+			System.out.println("Original input file read succesfully!");
+		}
+		
+		String line = null;
+		int counter = 0;
+		System.out.println("Starting to split the input file into individual files!");
+		try {
+			while((line = buffer.readLine()) != null) {
+				if(checkNewFile(line) && counter == 0) {
+					outputFile = new BufferedWriter(new FileWriter(line.replace("*/", "")));
+					counter++;
+					continue;
+				}
+				else if(checkNewFile(line) && counter != 0) {
+					outputFile.close();
+					outputFile = new BufferedWriter(new FileWriter(line.replace("*/", "")));
+					continue;
+				}
+				outputFile.write(line + "\n");
+			}
+			outputFile.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("Finished splitting the origianl file into individual files!");
+		}
 	}
 
 }
