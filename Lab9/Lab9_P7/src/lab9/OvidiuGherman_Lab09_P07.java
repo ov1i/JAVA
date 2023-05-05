b26f63fe3bce28c0a7a36c63c32573fd36f07f78
Ovi <govidiu15@gmail.com>
2023-05-05 23:31:07 +0300

    Lab -COMPLETE

--- Lab9/Lab9_P7/src/lab9/OvidiuGherman_Lab09_P07.java
+++ Lab9/Lab9_P7/src/lab9/OvidiuGherman_Lab09_P07.java
@@ -1,5 +1,15 @@
 package lab9;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 /* 7. 
  * There is an urn which contains 1000 losing tickets and 1 winning ticket. There are N people around the urn (N is read
  * from the keyboard) which simultaneously extract tickets from it. The tickets are not placed back into the urn. When one
@@ -7,10 +17,62 @@ package lab9;
  * Alternatives: 1) the persons extract the tickets in a predefined order; 2) the tickets are placed back into the urn
  */
 
-public class OvidiuGherman_Lab09_P07 {
+class checkWin implements Callable<Integer> {
+	
+	private static volatile boolean found = false;
+	private List<Boolean> urn;
+	private Random random; 
+	final private int threadId;
+	
+	public checkWin(List<Boolean> urn, Random random, int id) {
+		this.threadId = id;
+		this.urn = urn;
+		this.random = random;
+	}
+	@Override
+	public Integer call() throws Exception {
+	    while (true) {
+	        if (found) {
+	            break;
+	        }
+	        int index = random.nextInt(0, urn.size());
+	        if (urn.get(index)) {  
+	        	System.out.println("Winner winner chicken dinner -> Thread: " + this.threadId);
+	            found = true;	           
+	        } else {
+	            urn.remove(index);
+	        }
+	        if (urn.size() == 0) {
+	            break;
+	        }
+	    }
+	    return null;
+	}
 
+	
+}
+
+public class OvidiuGherman_Lab09_P07 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		List<Boolean> urn = new ArrayList<>(Collections.nCopies(1000, false));
+		urn.add(true);
+		Collections.shuffle(urn);
+
+
+		System.out.print("Please enter the number of persons: ");
+		int ppl = scanner.nextInt();	
+		scanner.close();
 		
+		ExecutorService people = Executors.newFixedThreadPool(ppl);
+		
+		for(int i = 0; i < ppl;i++) {
+			people.submit(new checkWin(urn,random,i));
+		}
+		
+		
+		people.shutdown();
 	}
 
 }
